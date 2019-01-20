4a1261b071998c88384d6b19495431e1a255a77f
John Warburton <john@johnwarburton.net>
2019-01-20 09:55:35 +0000

    Add histogram, and control frame rate

--- audiometer.py
+++ audiometer.py
@@ -1,4 +1,6 @@
 """ Audio Display """
+## Uses the MPV player to display useful information about incoming stereo audio
+## Have a look here for a Windows download. https://github.com/Warblefly/MultimediaTools-mingw-w64
 
 from string import Template
 import argparse, tempfile, subprocess, json, pprint
@@ -10,48 +12,50 @@ def ffmpegEscape(input):
 ## IMPORTANT -- CONFIGURATION ##
 ## PLEASE ENTER YOUR CONFIGURATION VALUES BELOW
 ##
-##   The FULL PATH to your FFmpeg binary
+##   The FULL PATH to your MPV binary
 ##   Please keep the 'r' at the front
 
 MPV = r"c:/Program Files/ffmpeg/bin/"
 
-##   The FULL PATHNAME of your font for the timecode
-
-FONT = ffmpegEscape(r"c:/windows/fonts/arial.ttf")
-
 parser = argparse.ArgumentParser(description='Audio monitor. Requires one audio input.')
 parser.add_argument('filename', metavar='file', help='Audio file to be displayed.')
+parser.add_argument('-r', default=50, type=int, help='Frame rate to display.')
 
 args = parser.parse_args()
 filename_raw = args.filename
+rate = args.r
 
 filename = ffmpegEscape(filename_raw)
 
-LAVFI = "[aid1]asplit=7[a][b][c][d][e][f][g];" + \
-        "[a]avectorscope=size=480x480:zoom=2:draw=line,drawgrid=240:240:color=gray[z];" + \
+
+LAVFI = "[aid1]asplit=8[a][b][c][d][e][f][g][i];" + \
+        "[a]avectorscope=size=360x360:zoom=0.1:swap=1:draw=line:rate=" + str(rate) + ",drawgrid=180:180:color=gray[z];" + \
         "[b]ebur128=video=1:meter=18[q][ao];" + \
-        "[q]scale=480:480[y];[z][y]hstack[w];" + \
-        "[c]showfreqs=fscale=lin:win_size=w4096:cmode=separate:size=480x480:minamp=1e-009," + \
-        "drawgrid=x=0:y=479:w=100:h=60:color=gray[u];" + \
-        "[d]showspectrum=size=480x480:overlap=1:slide=scroll:scale=5thrt:mode=combined[t];" + \
+        "[q]scale=360:360[y];[z][y]hstack[w];" + \
+        "[c]showfreqs=fscale=lin:win_size=w4096:cmode=separate:size=360x360," + \
+        "drawgrid=x=0:y=0:w=90:h=180:color=gray,fps=fps=" + str(rate) + "[u];" + \
+        "[d]showspectrum=overlap=1:slide=scroll:scale=5thrt:mode=combined:legend=1:fps=" + str(rate) + "," + \
+        "scale=360:360:lanczos[t];" + \
         "[t][u]hstack[v];" + \
-        "[e]showvolume=w=960:h=50:t=0:f=0.9:dm=1," + \
-        "drawtext=font=Arial:fontsize=24:text='1':x=47:y=40:fontcolor=white," + \
-        "drawtext=font=Arial:fontsize=24:text='2':x=77:y=40:fontcolor=white," + \
-        "drawtext=font=Arial:fontsize=24:text='3':x=128:y=40:fontcolor=white," + \
-        "drawtext=font=Arial:fontsize=24:text='4':x=206:y=40:fontcolor=cyan," + \
-        "drawtext=font=Arial:fontsize=24:text='5':x=330:y=40:fontcolor=white," + \
-        "drawtext=font=Arial:fontsize=24:text='6':x=528:y=40:fontcolor=red," + \
-        "drawtext=font=Arial:fontsize=24:text='7':x=842:y=40:fontcolor=pink[s];" + \
-        "[f]showcqt=size=960x180:bar_g=7:timeclamp=0.5[r];" + \
-        "[g]aphasemeter=size=960x40:mpc=red[h][p];" + \
+        "[e]showvolume=rate=" + str(rate) + ":w=720:h=40:t=0:f=0.9:dm=1," + \
+        "drawtext=font=Arial:fontsize=16:text='1':x=35:y=34:fontcolor=white," + \
+        "drawtext=font=Arial:fontsize=16:text='2':x=58:y=34:fontcolor=white," + \
+        "drawtext=font=Arial:fontsize=16:text='3':x=96:y=34:fontcolor=white," + \
+        "drawtext=font=Arial:fontsize=16:text='4':x=155:y=34:fontcolor=cyan," + \
+        "drawtext=font=Arial:fontsize=16:text='5':x=248:y=34:fontcolor=white," + \
+        "drawtext=font=Arial:fontsize=16:text='6':x=396:y=34:fontcolor=red," + \
+        "drawtext=font=Arial:fontsize=16:text='7':x=631:y=34:fontcolor=pink," + \
+        "scale=720:-1[s];" + \
+        "[f]showcqt=size=720x120:bar_g=7:bar_t=0.25:timeclamp=0.5:rate=" + str(rate/2) + "[r];" + \
+        "[g]aphasemeter=size=720x39:rate=" + str(rate/2) + ":mpc=red[h][p];" + \
         "[h]anullsink;" + \
-        "[v][w][r][p][s]vstack=inputs=5,fps=50[vo]"
+        "[i]ahistogram=rate=" + str(rate) + ":size=720x80:rheight=1/2:acount=40:slide=scroll[x];" + \
+        "[v][w][r][p][x][s]vstack=inputs=6,setsar=1/1[vo]"
 
 dos_command = MPV + 'mpv.exe' + ' -v ' + ' --lavfi-complex "' + LAVFI + '" ' + filename_raw
 
 try:
-    subprocess.check_output(dos_command, stderr=subprocess.STDOUT)
+    test = str(subprocess.check_output(dos_command, stderr=subprocess.STDOUT)).split('\\n')
 except subprocess.CalledProcessError as e:
     print(e)
 
